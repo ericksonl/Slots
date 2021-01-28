908ffdb2f186772d8ebaab0846e7975d1d878851
Liam Erickson <71674831+ericksonl@users.noreply.github.com>
2021-01-28 12:46:59 -0800

    Update CasinoDriver.java

--- CasinoDriver.java
+++ CasinoDriver.java
@@ -1,3 +1,8 @@
+/*******************************
+ * Author: Liam Erickson 
+ * Date: March 13, 2020
+ ********************************/
+
 public class CasinoDriver {
     public static void main (String [] args) {
         Casino myCasino = new Casino("TexasBest");    
